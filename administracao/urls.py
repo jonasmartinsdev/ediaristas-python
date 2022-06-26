ff63a1373e458042da89681ad221cad46026c80c
jonasmartins <jonasmartinsrubim@gmail.com>
2022-06-26 17:20:29 -0300

    CRUD de serviços pronto :tada:

---
+++ administracao/urls.py
@@ -0,0 +1,11 @@
+
+
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+  path('servicos/cadastrar', cadastrar_servico, name='cadastrar_servico'),
+  path('servicos/listar', listar_servico, name='listar_servicos'),
+  path('servicos/editar/<int:id>', editar_servico, name='editar_servico'),
+]
